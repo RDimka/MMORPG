3e3226e19b566400a0eed5c2b84db9aad92235aa
R.Dmitriy <rdimka@mail.ru>
2023-03-21 23:58:08 +0500

    +models +views +sign

--- MMORPG/urls.py
+++ MMORPG/urls.py
@@ -18,7 +18,8 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-
+    path('', include('protect.urls')),
     path('board/', include('board.urls')),
-
+    path('sign/', include('sign.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
